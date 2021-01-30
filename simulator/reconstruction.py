f4f05caf6f2ed478793480868d31ee2e598190bd
JoBie97 <70331750+JoBie97@users.noreply.github.com>
2021-01-30 16:37:43 +0100

    Add files via upload

--- simulator/reconstruction.py
+++ simulator/reconstruction.py
@@ -16,8 +16,8 @@ of the class to use the reconstructor.reconstruct(detector_output) method.
 
 '''
 
-class Reconstructor(object):
 
+class Reconstructor(object):
 
     def __init__(self, event_data=None):
         self.event_data = None
@@ -32,16 +32,16 @@ class Reconstructor(object):
 
     def vector2event(self, vector):
         event = {'xpixel': vector[0], 'ypixel': vector[1], 'zpixel': vector[2], 't': vector[3]}
-        return event 
+        return event
 
     def vector_reconstruct(self, vector1, vector2):
         vector_recon = vector2 - vector1
         vector_reprod = {'vx': 0, 'vy': 0, 'vz': 0, 't': 0, '|v|': 0}
         vector_reprod['vx'] = vector_recon[0]/vector_recon[3]
         vector_reprod['vy'] = vector_recon[1]/vector_recon[3]
-        vector_reprod['vz'] = vector_recon[2]/vector_recon[3]        
+        vector_reprod['vz'] = vector_recon[2]/vector_recon[3]
         vector_reprod['t'] = vector1[3] - vector1[2]/vector_recon[2]*vector_recon[3]
-        vector_reprod['|v|'] = numpy.sqrt(vector_reprod['vx']**2 + vector_reprod['vy']**2 +vector_reprod['vz']**2)
+        vector_reprod['|v|'] = numpy.sqrt(vector_reprod['vx']**2 + vector_reprod['vy']**2 + vector_reprod['vz']**2)
         return vector_reprod
 
     def reconstruct(self, data_list):
@@ -50,5 +50,5 @@ class Reconstructor(object):
             detections = len(list)
             first_det = self.event2vector(list[0])
             final_det = self.event2vector(list[detections - 1])
-            reconstructed_data.append(self.vector_reconstruct(first_det, final_det))                                      
+            reconstructed_data.append(self.vector_reconstruct(first_det, final_det))
         return reconstructed_data
