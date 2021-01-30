2b456d9317c071d067b824f9846f32368628c4e6
JoBie97 <70331750+JoBie97@users.noreply.github.com>
2021-01-30 16:33:51 +0100

    Add files via upload

--- simulator/reconstruction.py
+++ simulator/reconstruction.py
@@ -10,31 +10,30 @@ detector space (z =[30, 50]) at all time:
     dictionarys into a list of numpy arrays. Substracts related event vectors to
     recreate a flight vector.
     Conversion into a dictionary of the format of the actual particles flight vectors.
-    
+
 Use: create object reconstructor = Reconstructor()
 of the class to use the reconstructor.reconstruct(detector_output) method.
 
 '''
 
 class Reconstructor(object):
-    
+
+
     def __init__(self, event_data=None):
         self.event_data = None
-        
 
 # Methods:
-    
+
     def event2vector(self, event):
         event_vector = numpy.array([])
         for value in event.values():
             event_vector = numpy.append(event_vector, value)
         return event_vector
-    
+
     def vector2event(self, vector):
-        
         event = {'xpixel': vector[0], 'ypixel': vector[1], 'zpixel': vector[2], 't': vector[3]}
         return event 
-    
+
     def vector_reconstruct(self, vector1, vector2):
         vector_recon = vector2 - vector1
         vector_reprod = {'vx': 0, 'vy': 0, 'vz': 0, 't': 0, '|v|': 0}
@@ -44,8 +43,7 @@ class Reconstructor(object):
         vector_reprod['t'] = vector1[3] - vector1[2]/vector_recon[2]*vector_recon[3]
         vector_reprod['|v|'] = numpy.sqrt(vector_reprod['vx']**2 + vector_reprod['vy']**2 +vector_reprod['vz']**2)
         return vector_reprod
-    
-    
+
     def reconstruct(self, data_list):
         reconstructed_data = []
         for list in data_list:
