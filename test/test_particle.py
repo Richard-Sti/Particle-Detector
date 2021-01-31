61ed33f710ec9e2bbbe0bebb5562bce2be30748d
Toan <Toan.Nguyen@campus.lmu.de>
2021-01-31 19:13:38 +0100

    fixed mistakes

--- test/test_particle.py
+++ test/test_particle.py
@@ -7,7 +7,7 @@ THETA_MAX = 10
 RATE = 10
 
 
-@pytest.mark.parametrize(particle_number, [1, 10, 20])
+@pytest.mark.parametrize('particle_number', [1, 10, 20])
 def test_particle_attributes(particle_number):
     """Tests whether particle returns correct value of a unit in Generator."""
     # Setup the generator
@@ -15,20 +15,20 @@ def test_particle_attributes(particle_number):
     events = source.observe(T)
     # Setup the Particle
     particle = Particle(events)
-    out = particle.give_particle(particle_number)
+    out = Particle.give_particle(particle_number)
     assert out == events[particle_number-1]
 
 
-@pytest.mark.parametrize(number_begin, [0, 5, 10])
-@pytest.mark.parametrize(number_end, [5, 10, 20])
-def test_particle_attributes(particle_number):
+@pytest.mark.parametrize('number_begin', [0, 5, 10])
+@pytest.mark.parametrize('number_end', [5, 10, 20])
+def test_particle_attributes(number_begin, number_end):
     """Tests whether particles return correct values of Generator."""
     # Setup the generator
     source = PoissonSource(THETA_MAX, rate=RATE)
     events = source.observe(T)
     # Setup the Particle
     particle = Particle(events)
-    out = particle.give_range_of_particle(number_begin, number_end)
+    out = Particle.give_range_of_particle(number_begin, number_end)
     N = len(events)
 
     if N > number_end:
